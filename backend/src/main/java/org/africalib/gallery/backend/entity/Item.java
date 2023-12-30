51ceda7cbed62c3e601c7a01c9b9c3b512bb0d61
SionLee21 <sionl1030@gmail.com>
2023-12-31 02:59:02 +0900

    six

---
+++ backend/src/main/java/org/africalib/gallery/backend/entity/Item.java
@@ -0,0 +1,14 @@
+package org.africalib.gallery.backend.entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name="items")
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(length = 50, nullable = false)
+    private String name;
+}
